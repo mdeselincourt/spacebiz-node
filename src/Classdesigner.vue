f41a43708aee7a293b0f8879991a743ff760184c
Mike de Selincourt <m.de.selincourt@googlemail.com>
2018-12-17 20:45:17 +0000

    Nicer designer

--- src/Classdesigner.vue
+++ src/Classdesigner.vue
@@ -8,18 +8,19 @@
 			  </p>
 		
 			<ul>
-				<li>Engines<input id="enginetonnage" v-model.number="enginetonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="250" SIZE="20"/></li>
-				<li>Power<input id="powertonnage" v-model.number="powertonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="500" SIZE="10"/></li>
-				<li>Command<input id="commandtonnage" v-model.number="commandtonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="250" SIZE="10"/></li>
-				<li>Habitation<input id="habitationtonnage" v-model.number="habitationtonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="750" SIZE="10"/></li>
-				<li>Fuel<input id="fueltonnage" v-model.number="fueltonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="250" SIZE="10"/></li>
-				<li>Ammunition<input id="ammunitiontonnage" v-model.number="ammunitiontonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="250" SIZE="10"/></li>
-				<li>Stores<input id="storestonnage" v-model.number="storestonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="250" SIZE="10"/></li>
-				<li>Weaponry<input id="weaponrytonnage" v-model.number="weaponrytonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="250" SIZE="10"/></li>
+				<li><input id="enginetonnage" v-model.number="enginetonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="250" SIZE="20"/> t Engines</li>
+				<li><input id="powertonnage" v-model.number="powertonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="500" SIZE="10"/> t Power</li>
+				<li><input id="commandtonnage" v-model.number="commandtonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="250" SIZE="10"/> t Command</li>
+				<li><input id="habitationtonnage" v-model.number="habitationtonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="750" SIZE="10"/> t Habitation</li>
+				<li><input id="fueltonnage" v-model.number="fueltonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="250" SIZE="10"/> t Fuel</li>
+				<li><input id="ammunitiontonnage" v-model.number="ammunitiontonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="250" SIZE="10"/> t Ammunition</li>
+				<li><input id="storestonnage" v-model.number="storestonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="250" SIZE="10"/> t Stores</li>
+				<li><input id="weaponrytonnage" v-model.number="weaponrytonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" VALUE="250" SIZE="10"/> t Weaponry</li>
 			</ul>
 			
 			<p>Total tonnage: {{ totaltonnage }} t</p>
-			
+			<p>Length: {{ length }} m</p>
+
 			<p>
 				<input type="submit">  
 			</p> 
@@ -66,6 +67,9 @@
       totaltonnage() {
         //return typeof this.enginetonnage;
         return this.enginetonnage + this.powertonnage + this.commandtonnage + this.habitationtonnage + this.fueltonnage + this.ammunitiontonnage + this.storestonnage + this.weaponrytonnage;
+      },
+      length() {
+          return (this.enginetonnage + this.powertonnage + this.commandtonnage + this.habitationtonnage + this.fueltonnage + this.ammunitiontonnage + this.storestonnage + this.weaponrytonnage) * (110/2750);
       }
     }
   }
