8b1338c0ae3ecf610b60e0dd84148218150aea4d
Mike de Selincourt <m.de.selincourt@googlemail.com>
2018-12-19 23:06:48 +0000

    Refactored to use a class instance as the main data object

--- spacebiz-backend/logic/Spacebiz.js
+++ spacebiz-backend/logic/Spacebiz.js
@@ -1,12 +1,64 @@
+// A module that is just a literal:
 //export default "Hello from Spacebiz.js";
 
+// A module that is a single anonymous function:
 // export default function() { 
 //      // Can be imported into a vue component and addressed e.g. inside data: or mounted()
 //      return "Hello from Spacebiz.js anonymous function";
 // }
 
-export default { 
-    calculateShipRole() {
-        return "Probably a Frigate"; 
+// A module that is an anonymous object which only contains functions:
+// export default { 
+//     calculateShipRole() {
+//         return "Probably a Frigate"; 
+//     },
+//}
+
+// A module that is a class containing ES6 static functions
+export default class Spacebiz {
+
+    // Here's an example constructor
+    // constructor(name, fierce){
+    //   this._name = name;
+    //   this._fierce = fierce;
+    // }
+    
+    // Here's a static method
+    //static calculateShipRole() {
+    //    return "Maybe a Frigate?";
+    //}
+}
+
+export class ShipClass {
+
+    constructor () {
+        
+        this.name = "Formidable",
+        this.enginetonnage =  250,
+        this.powertonnage =  500,
+        this.commandtonnage =  250,
+        this.habitationtonnage =  750,
+        this.fueltonnage =  250,
+        this.ammunitiontonnage =  250,
+        this.storestonnage =  250,
+        this.weaponrytonnage =  250
+    }
+
+    get tonnage() {
+        return this.enginetonnage + this.powertonnage + this.commandtonnage + this.habitationtonnage + this.fueltonnage + this.ammunitiontonnage + this.storestonnage + this.weaponrytonnage;
+    } 
+
+    get role() {
+        
+        if (this.tonnage < 1350) { return "Corvette"; }
+        else if (this.tonnage < 2500) { return "Frigate"; }
+        else if (this.tonnage < 8000) { return "Destroyer"; }
+        else if (this.tonnage < 12000) { return "Cruiser"; }
+        else { return "Capital"; }
+        
+    }
+
+    static calculateLength(tonnage) {
+        return Math.round(10 * (tonnage ^ 0.33));
     }
 }
--- spacebiz-frontend/src/components/Classdesigner.vue
+++ spacebiz-frontend/src/components/Classdesigner.vue
@@ -4,19 +4,20 @@
 			<h1>Class Designer</h1>
 			<p>
 				<label for="classname">Name:</label>
-				<input id="classname" v-model="shipclass.name" value="">
-				<input id="classrole" v-model="shipclass.role" value="">		
+				<input id="classname" v-model="shipclass.name" value=""/>
+				<input id="classrole" v-model="shipclass.role" value=""/>		
+				<input id="totaltonnagedebug" v-model="totaltonnage"/>
 			</p>
 	
 			<ul>
-				<li><input id="enginetonnage" v-model.number="shipclass.enginetonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" SIZE="20"/> t Engines</li>
-				<li><input id="powertonnage" v-model.number="shipclass.powertonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" SIZE="10"/> t Power</li>
-				<li><input id="commandtonnage" v-model.number="shipclass.commandtonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" SIZE="10"/> t Command</li>
-				<li><input id="habitationtonnage" v-model.number="shipclass.habitationtonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" SIZE="10"/> t Habitation</li>
-				<li><input id="fueltonnage" v-model.number="shipclass.fueltonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" SIZE="10"/> t Fuel</li>
-				<li><input id="ammunitiontonnage" v-model.number="shipclass.ammunitiontonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" SIZE="10"/> t Ammunition</li>
-				<li><input id="storestonnage" v-model.number="shipclass.storestonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" SIZE="10"/> t Stores</li>
-				<li><input id="weaponrytonnage" v-model.number="shipclass.weaponrytonnage" TYPE="NUMBER" MIN="50" MAX="1000" STEP="50" SIZE="10"/> t Weaponry</li>
+				<li><input id="enginetonnage" v-model.number="shipclass.enginetonnage" TYPE="NUMBER" MIN="50" MAX="10000" STEP="50" SIZE="20"/> t Engines</li>
+				<li><input id="powertonnage" v-model.number="shipclass.powertonnage" TYPE="NUMBER" MIN="50" MAX="10000" STEP="50" SIZE="10"/> t Power</li>
+				<li><input id="commandtonnage" v-model.number="shipclass.commandtonnage" TYPE="NUMBER" MIN="50" MAX="10000" STEP="50" SIZE="10"/> t Command</li>
+				<li><input id="habitationtonnage" v-model.number="shipclass.habitationtonnage" TYPE="NUMBER" MIN="50" MAX="10000" STEP="50" SIZE="10"/> t Habitation</li>
+				<li><input id="fueltonnage" v-model.number="shipclass.fueltonnage" TYPE="NUMBER" MIN="50" MAX="10000" STEP="50" SIZE="10"/> t Fuel</li>
+				<li><input id="ammunitiontonnage" v-model.number="shipclass.ammunitiontonnage" TYPE="NUMBER" MIN="50" MAX="10000" STEP="50" SIZE="10"/> t Ammunition</li>
+				<li><input id="storestonnage" v-model.number="shipclass.storestonnage" TYPE="NUMBER" MIN="50" MAX="10000" STEP="50" SIZE="10"/> t Stores</li>
+				<li><input id="weaponrytonnage" v-model.number="shipclass.weaponrytonnage" TYPE="NUMBER" MIN="50" MAX="10000" STEP="50" SIZE="10"/> t Weaponry</li>
 			</ul>
 		
 			<ul>
@@ -36,32 +37,38 @@
 	// import axios from 'axios'; // For HTTP!
 	// import json from 'json'; // For strinfigying for debugging/logging
 	
-	import Spacebiz from '../../../spacebiz-backend/logic/Spacebiz.js'; // HERE'S HOW I COULD IMPORT A NODE MODULE
+	//     { Spacebiz, 
+	import { ShipClass } from '../../../spacebiz-backend/logic/Spacebiz.js'; // HERE'S HOW I COULD IMPORT A MODULE
 
 	// Configure this SFC
 	export default {
 		name: 'classdesigner',
 		data () { 
-			return { // Since the returned object is anonymous I think it's best always to have named entities inside it
-				shipclass: {
-					name: "Formidable",
-					role: "",
-					// role: Logic(), // Works if Logic is just a single anonymous function
-					enginetonnage: 250,
-					powertonnage: 500,
-					commandtonnage: 250,
-					habitationtonnage: 750,
-					fueltonnage: 250,
-					ammunitiontonnage: 250,
-					storestonnage: 250,
-					weaponrytonnage: 250
-				},
-				// logic: logic // HERE'S HOW I COULD TURN AN IMPORTED MODULE INTO A DATA OBJECT
-			}
+
+			return { shipclass: new ShipClass() };
+			
+			//{ // Since the returned object is anonymous I think it's best always to have named entities inside it
+				
+				
+				// shipclass: {
+				// 	name: "Formidable",
+				// 	role: "",
+				// 	// role: Logic(), // Works if Logic is just a single anonymous function
+				// 	enginetonnage: 250,
+				// 	powertonnage: 500,
+				// 	commandtonnage: 250,
+				// 	habitationtonnage: 750,
+				// 	fueltonnage: 250,
+				// 	ammunitiontonnage: 250,
+				// 	storestonnage: 250,
+				// 	weaponrytonnage: 250
+				// }
+				// , logic: logic // HERE'S HOW I COULD TURN AN IMPORTED MODULE INTO A DATA OBJECT
+			//}
 		}, 
 		mounted() {
-			//this.shipclass.role = Logic(); // works if module is just an anonymous function
-			this.shipclass.role = Spacebiz.calculateShipRole(); // works if module is an object with methods
+			// this.shipclass.role = Logic(); // works if module is just an anonymous function
+			// this.shipclass.role = ShipClass.calculateShipRole(this.shipclass.enginetonnage + this.shipclass.powertonnage + this.shipclass.commandtonnage + this.shipclass.habitationtonnage + this.shipclass.fueltonnage + this.shipclass.ammunitiontonnage + this.shipclass.storestonnage + this.shipclass.weaponrytonnage); // works if module is an object with methods
 		},
 		methods: {
 			handleSubmit() {
@@ -71,23 +78,22 @@
 		},
 		computed: {
 			totaltonnage() {
+			
 				// A Flower corvette is maybe 1000 t
 				// River-class frigate from 1370
 				// Tacoma WW2 Frigate from 1500t
-				// 40s JKN-class destroyer 1700        
+				// OP class 1650 t
+				// 40s JKN, QR, WZ, C, ST, UK class destroyer 1700        
 				// 40s Tribal 1850 t
 				// M class 1920 t
-				// OP class 1650 t
-				// QR, WZ, C class 1700 t
-				// UV class 1777 t
-				// ST class 1780 t
-				// Weapon class 2000t
-				// Battle class 2500t
+				// Weapon class destroyer 2000t
+				// Battle class destroyer 2500t
 				// A 70s FRIGATE masses about 2750 
-				// Daring class 2800 t
+				// Daring class destroyer 2800 t
 				// 70s? Destroyer 3500
 				// 40s Dido class cruiser 5800 t !
 				// 60s County class new-gen missile destroyer 6200 t !
+				// Future F125 FRIGATE 7200 t
 				// 80s Arleigh Burke destroyer 8000t 
 				// York heavy cruiser 8,250 (ermm?!)
 				// Sejong the Great large modern destroyer 8,500
@@ -100,8 +106,13 @@
 				//return typeof this.enginetonnage;
 			
 				// ATTEMPTING TO OUTFACTOR TO A MODULE
-				return this.shipclass.enginetonnage + this.shipclass.powertonnage + this.shipclass.commandtonnage + this.shipclass.habitationtonnage + this.shipclass.fueltonnage + this.shipclass.ammunitiontonnage + this.shipclass.storestonnage + this.shipclass.weaponrytonnage;
-				
+				//return this.shipclass.enginetonnage + this.shipclass.powertonnage + this.shipclass.commandtonnage + this.shipclass.habitationtonnage + this.shipclass.fueltonnage + this.shipclass.ammunitiontonnage + this.shipclass.storestonnage + this.shipclass.weaponrytonnage;
+
+				// NO YOU MAY NOT HAVE SIDE EFFECTS IN YOUR COMPUTED FUNCTIONS that's naughty and Lint will complain.
+				//var tonnage = this.shipclass.enginetonnage + this.shipclass.powertonnage + this.shipclass.commandtonnage + this.shipclass.habitationtonnage + this.shipclass.fueltonnage + this.shipclass.ammunitiontonnage + this.shipclass.storestonnage + this.shipclass.weaponrytonnage; 
+
+				//return this.shipclass.tonnage; // works if module is an object with methods
+				return this.shipclass.tonnage;
 			},
 			length() {
 					return Math.round((this.shipclass.enginetonnage + this.shipclass.powertonnage + this.shipclass.commandtonnage + this.shipclass.habitationtonnage + this.shipclass.fueltonnage + this.shipclass.ammunitiontonnage + this.shipclass.storestonnage + this.shipclass.weaponrytonnage) 
