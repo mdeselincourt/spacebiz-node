9ab8f4428423c6bbd2f739eee25bce54fa3f8697
Mike de Selincourt <m.de.selincourt@googlemail.com>
2018-12-26 22:08:42 +0000

    Switching to a routed frontend

--- spacebiz-frontend/src/components/Spacebizuserbar.vue
+++ spacebiz-frontend/src/components/Spacebizuserbar.vue
@@ -1,19 +1,58 @@
 <template>
     <div id="spacebizuserbar">
-       <p>You are: {{ username }}. You have Ⴥ{{ balance }}</p>
+        <div id="userheader">
+        <p>You are: {{ charactername }}. You have Ⴥ{{ balance }}. Authdebug is {{ authDebug }} </p>
+        </div>
+
+        <div class="tutorialedge-login-box">
+            <h4>Register</h4>
+            <div class="card-panel red darken-2" v-if="error != null"><span class="white-text">{{ error.message }}</span></div>
+            <p>Don't have an account? Register for one ("using your Google account"...?!?)</p>
+            <form @submit.prevent="authenticate">
+                <div class="input-field">
+                    <input id="username" type="text" class="validate" v-model="username" required>
+                    <label for="username">Username</label>
+                </div>
+                <div class="input-field">
+                    <input id="email" type="text" class="validate" v-model="email" required>
+                    <label for="email">Email</label>
+                </div>
+                <div class="input-field">
+                    <input id="password" type="password" class="validate" v-model="pass" required>
+                    <label for="password">Password</label>
+                </div>
+                <div class="center-align">
+                    <button class="btn btn-default btn-large">Register</button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
-    //module.exports = {
+
     export default {
         data: function() {
+            
             return {
-                username: 'Brapp Zannigan',
-                balance: 9000
+                charactername: 'Brapp Zannigan',
+                balance: 9000,
+                
+                // This was written based on the "registration component" in the tutorial
+                authDebug: 'Nothing',
+                username: '',
+                pass: '',
+                email: '',
+                error: null
+            }
+        },
+        methods: {
+            authenticate() {
+                this.authDebug = "Authenticate()...";
             }
         }
     }
+
 </script>
 
 <style scoped>
--- spacebiz-frontend/src/main.js
+++ spacebiz-frontend/src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+// Front end app:
+import Vue from 'vue' // Vue framework
+import App from './App.vue' // My root Vue component; it contains all the others
 
+// Not sure what this does
 Vue.config.productionTip = false
 
+// 'mount' the Vue app.
 new Vue({
   render: h => h(App),
 }).$mount('#app')
